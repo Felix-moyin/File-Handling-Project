7bbbaf179bf18930aa6e3f2a9f63a6c4d604efcd
Felix-moyin <ogunjobifelix3@gmail.com>
2026-08-24 13:33:12 +0100

    Add an explanation of file

---
+++ file handling.py
@@ -0,0 +1,15 @@
+"""
+What is a File?
+A File is a named location on a computer used to store data permanently.
+
+Unlike variables, the data inside a file remains available even after the program has been closed
+
+Examples of file include:
+students.txt
+scores.csv
+setting.json
+report.pdf
+
+"""
+
+# Reading Files (The Manual Way)
